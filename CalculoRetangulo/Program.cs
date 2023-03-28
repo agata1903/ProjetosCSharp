a36c025c37184689734da495bdf63c24f71a18c8
Ágata <59833071+agata1903@users.noreply.github.com>
2023-03-28 11:36:54 -0300

    Add files via upload

---
+++ CalculoRetangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+            Console.WriteLine("Entre a largura e a altura do retângulo");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Área: " + r.Area().ToString("F2"));
+            Console.WriteLine("Perímetro: " + r.Perimetro().ToString("F2"));
+            Console.WriteLine("Diagonal: " + r.Diagonal().ToString("F2"));
+        }
+    }
+}
